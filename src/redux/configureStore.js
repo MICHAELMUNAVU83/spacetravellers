e6455d35af2afa261a88a35fc6f7ef11ed4f3241
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-25 15:00:32 +0100

    Fetch dragon api

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import misssionsReducer from './missions';
 import rocketReducer from './rocket';
+import dragonReducer from './dragons';
 
 const store = configureStore({
   reducer: {
     missions: misssionsReducer,
     rockets: rocketReducer,
+    dragons: dragonReducer,
   },
 });
 export default store;
