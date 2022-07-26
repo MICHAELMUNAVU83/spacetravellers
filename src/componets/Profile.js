b864cf89f53acee713c5bfc92f758faeb7cad45c
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-26 10:39:42 +0100

    Add reserved rockets to the profile

--- src/componets/Profile.js
+++ src/componets/Profile.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Profile() {
+  const rockets = useSelector((state) => state.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+  const newRockets = reservedRockets.map((reservedRocket) => (
+    <div key={reservedRocket.id}>
+      {' '}
+      {reservedRocket.rocket_name}
+      {' '}
+    </div>
+  ));
   return (
     <div>
-      <h1>its our profile</h1>
+      <h2>
+        {' '}
+        {newRockets}
+      </h2>
     </div>
   );
 }
